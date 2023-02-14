a87cabc8aec7795e995feb435142e803300d40c8
FacundoCastillo12 <facuelfacha15@gmail.com>
2023-02-14 20:45:54 -0300

    actualizar tarea usando POO
    Se actualiza la tarea usando lo que aprendimos de clase 15 y se lo ordena siguiendo las reglas de ESLINT

--- src/main.js
+++ src/main.js
@@ -1,92 +1,134 @@
+/* eslint-disable func-names */
 /// <reference types="jquery" />
 
-document.querySelector('#ingresar').onclick = function () {
-	let moneda = seleccionarMoneda('eleccion');
-	mostrarTipoCambios(moneda);
-};
-document.querySelector('#convertir').onclick = function () {
-	let monedaBase = seleccionarMoneda('base');
-	let monedaACambiar = seleccionarMoneda('cambiar');
-	let cantidad = document.querySelector('#dinero-ingresado');
-	convertirMoneda(monedaBase, monedaACambiar, cantidad.value);
-};
-document.querySelector;
-function mostrarTipoCambios(moneda) {
-	fetch(
-		`https://v6.exchangerate-api.com/v6/502ae78ce62f17de5f000114/latest/${moneda}`
-	)
-		.then((respuesta) => respuesta.json())
-		.then((respuestaJSON) => {
-			$('h2').text(
-				`Ultima actualizacion: ${ordenarFecha(
-					respuestaJSON.time_last_update_utc
-				)} en base ${respuestaJSON.base_code}`
-			);
-			$('li').remove();
-			Object.keys(respuestaJSON.conversion_rates).forEach((moneda) => {
-				$('#lista-moneda').append(
-					$(
-						`<li ${'class="list-group-item"'}>${moneda}: ${
-							respuestaJSON.conversion_rates[moneda]
-						}</li>`
-					)
-				);
-			});
-		})
-		.catch((error) => console.log('error', error));
+class Moneda {
+  constructor(datosMoneda) {
+    this.monedaBase = datosMoneda.base_code;
+    this.monedaObjetivo = datosMoneda.target_code;
+    this.resultadoConversion = datosMoneda.conversion_result;
+    this.tasaConversion = datosMoneda.conversion_rate;
+    this.tasasConversion = datosMoneda.conversion_rates;
+    this.ultimaActualizacion = datosMoneda.time_last_update_utc;
+  }
 }
 
-function convertirMoneda(monedaBase, monedaACambiar, cantidad) {
-	fetch(
-		`https://v6.exchangerate-api.com/v6/502ae78ce62f17de5f000114/pair/${monedaBase}/${monedaACambiar}/${cantidad}`
-	)
-		.then((respuesta) => respuesta.json())
-		.then((respuestaJSON) => {
-			$('h2').text(
-				`Ultima actualizacion: ${ordenarFecha(
-					respuestaJSON.time_last_update_utc
-				)} convertir de ${respuestaJSON.base_code} a ${
-					respuestaJSON.target_code
-				}`
-			);
-			$('li').remove();
-			$('#lista-moneda').append(
-				$(
-					`<li class="list-group-item">Tasa de cambio de ${respuestaJSON.conversion_rate}</li>`
-				)
-			);
-			$('#lista-moneda').append(
-				$(
-					`<li class="list-group-item">${respuestaJSON.base_code} a ${respuestaJSON.target_code}: ${respuestaJSON.conversion_result}</li>`
-				)
-			);
-		});
+async function conseguirInformacionTipos(monedaBase) {
+  const respuesta = await fetch(
+    `https://v6.exchangerate-api.com/v6/502ae78ce62f17de5f000114/latest/${monedaBase}`,
+  );
+  const respuestaJSON = respuesta.json();
+  return respuestaJSON;
+}
+async function conseguirInformacionMoneda(
+  monedaBase,
+  monedaACambiar,
+  cantidad,
+) {
+  const respuesta = await fetch(
+    `https://v6.exchangerate-api.com/v6/502ae78ce62f17de5f000114/pair/${monedaBase}/${monedaACambiar}/${cantidad}`,
+  );
+  const respuestaJSON = respuesta.json();
+  return respuestaJSON;
 }
 
+function mostrarTiposDeCambios(tiposMonedas) {
+  const $listaMoneda = document.querySelector('#lista-moneda');
+  Object.keys(tiposMonedas).forEach((moneda) => {
+    const $elemento = document.createElement('li');
+    $elemento.className = 'list-group-item';
+    $elemento.textContent = `${moneda}: ${tiposMonedas[moneda]}`;
+    $listaMoneda.appendChild($elemento);
+  });
+}
 function ordenarFecha(fecha) {
-	if (/00:00:01/.test(fecha)) {
-		return fecha.replace('00:00:01 +0000', '');
-	} else if (/00:00:02/.test(fecha)) {
-		return fecha.replace('00:00:02 +0000', '');
-	} else if (/00:00:03/.test(fecha)) {
-		return fecha.replace('00:00:03 +0000', '');
-	}
+  if (/00:00:01/.test(fecha)) {
+    return fecha.replace('00:00:01 +0000', '');
+  }
+  if (/00:00:02/.test(fecha)) {
+    return fecha.replace('00:00:02 +0000', '');
+  }
+  if (/00:00:03/.test(fecha)) {
+    return fecha.replace('00:00:03 +0000', '');
+  }
+  return '';
 }
+function actualizarTitulo(conversion, moneda) {
+  if (conversion === true) {
+    const $actualizacion = document.querySelector('h2');
+    $actualizacion.textContent = `Ultima actualizacion: ${ordenarFecha(
+      moneda.ultimaActualizacion,
+    )} convertir de ${moneda.monedaBase} a ${moneda.monedaObjetivo}`;
+  }
+  if (conversion === false) {
+    const $actualizacion = document.querySelector('h2');
+    $actualizacion.textContent = `Ultima actualizacion ${ordenarFecha(
+      moneda.ultimaActualizacion,
+    )} en base a ${moneda.monedaBase}`;
+  }
+}
+function eliminarElementosLi() {
+  const $elementos = document.querySelectorAll('li');
+  for (let i = 0; i < $elementos.length; i += 1) {
+    $elementos[i].remove();
+  }
+}
+function mostrarMonedasConversion(moneda) {
+  actualizarTitulo(true, moneda);
+  eliminarElementosLi();
+  const $listaMoneda = document.querySelector('#lista-moneda');
+  const $nuevoElemento = document.createElement('li');
+  $nuevoElemento.className = 'list-group-item';
+  $nuevoElemento.textContent = `Tasa de cambio de ${moneda.tasaConversion}`;
+  const $segundoElemento = document.createElement('li');
+  $segundoElemento.className = 'list-group-item';
+  $segundoElemento.textContent = `${moneda.monedaBase} a ${moneda.monedaObjetivo}: ${moneda.resultadoConversion}`;
+
+  $listaMoneda.appendChild($nuevoElemento);
+  $listaMoneda.appendChild($segundoElemento);
+}
+async function convertirMoneda(monedaBase, monedaACambiar, cantidad) {
+  const respuestaJSON = await conseguirInformacionMoneda(
+    monedaBase,
+    monedaACambiar,
+    cantidad,
+  );
+  const moneda = new Moneda(respuestaJSON);
+  mostrarMonedasConversion(moneda);
+}
+
 function seleccionarMoneda(tipo) {
-	let $moneda = document.querySelector(`#moneda-${tipo}`);
-	return $moneda.value;
+  const $moneda = document.querySelector(`#moneda-${tipo}`);
+  return $moneda.value;
 }
 const $seleccionRadioBoton = document.querySelector('#radio');
 $seleccionRadioBoton.addEventListener('change', (event) => {
-	let $cambio = document.querySelector('#contenedor-cambio');
-	let $conversion = document.querySelector('#contenedor-conversion');
-	if (event.target.value === 'conversion') {
-		$conversion.classList.toggle('oculto');
-		$cambio.classList.toggle('oculto');
-	}
-	if (event.target.value === 'cambio') {
-		$cambio.classList.toggle('oculto');
-		$conversion.classList.toggle('oculto');
-	}
+  const $cambio = document.querySelector('#contenedor-cambio');
+  const $conversion = document.querySelector('#contenedor-conversion');
+  if (event.target.value === 'conversion') {
+    $conversion.classList.toggle('oculto');
+    $cambio.classList.toggle('oculto');
+  }
+  if (event.target.value === 'cambio') {
+    $cambio.classList.toggle('oculto');
+    $conversion.classList.toggle('oculto');
+  }
 });
 
+async function mostrarTiposCambios(monedaBase) {
+  const respuestaJSON = await conseguirInformacionTipos(monedaBase);
+  const tiposMonedas = new Moneda(respuestaJSON);
+  actualizarTitulo(false, tiposMonedas);
+  eliminarElementosLi();
+  mostrarTiposDeCambios(tiposMonedas.tasasConversion);
+}
+
+document.querySelector('#ingresar').onclick = function () {
+  const moneda = seleccionarMoneda('eleccion');
+  mostrarTiposCambios(moneda);
+};
+document.querySelector('#convertir').onclick = function () {
+  const monedaBase = seleccionarMoneda('base');
+  const monedaACambiar = seleccionarMoneda('cambiar');
+  const cantidad = document.querySelector('#dinero-ingresado');
+  convertirMoneda(monedaBase, monedaACambiar, cantidad.value);
+};
